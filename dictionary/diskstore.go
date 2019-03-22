c16cd1cde1f880dc31320d6b58754e7cd2487aae
Patrick G <geek1011@outlook.com>
2019-03-22 18:22:09 -0400

    Support concurrent dict reading, refactor size and offset code

--- dictionary/diskstore.go
+++ dictionary/diskstore.go
@@ -9,39 +9,41 @@ import (
 	"io"
 	"os"
 	"runtime/debug"
-	"sync"
 )
 
 // FileVer is the current compatibility level of saved Files.
-const FileVer = "DICT3\x00"
+const FileVer = "DICT4\x00"
 
 // File implements an efficient Store which is faster to initialize and uses a lot less memory (~15 MB total) than WordMap.
 //
-// There needs to be enough memory to store the whole index. There can be multiple concurrent readers of a single file, but each reader will read
-// one word at a time. Corrupt files will be detected during the read of the corrupted word (or the initialization in the case of index corruption)
+// There needs to be enough memory to store the whole index. Reading a dict is also completely thread-safe. Corrupt files
+// will be detected during the read of the corrupted word (or the initialization in the case of index corruption)
 // or during Verify.
 //
 // The dict file is stored in the following format:
 //
-//   +-----------+----------------------+---------------------------------------------------+---------------------------------------------------+
-//   |           |                      |  +--------------+--------------------------+      |                                                   |
-//   |  FileVer  |  idx offset (int64)  |  | size (int64) | zlib compressed Word gob | ...  |  zlib compressed idx map[string]int64 offset gob  |
-//   |           |                      |  +--------------+--------------------------+      |                                                   |
-//   +-----------+----------------------+---------------------------------------------------+---------------------------------------------------+
+//   +-----------+--------------+-------------------------------------------+------------+---------------------------------------------+
+//   |           |              |  +------+--------------------------+      |            |                                             |
+//   |  FileVer  |  idx offset  |  | size | zlib compressed Word gob | ...  |  idx size  |  zlib compressed idx map[string]offset gob  |
+//   |           |              |  +------+--------------------------+      |            |                                             |
+//   +-----------+--------------+-------------------------------------------+---- -------+---------------------------------------------+
+//
+//   All sizes and offsets are little-endian int64.
 //
 // The file is opened using the following steps:
 //
 // 1. The FileVer is read and checked. It must match exactly.
-// 2. The next int64 is read into a variable for idx offset.
+// 2. The idx offset is read.
 // 3. The file is seeked to the beginning plus the idx offset.
-// 4. The bytes for the idx are decompressed using zlib, and the resulting gob is decoded into an in-memory map[string]int64 of the words to offsets.
+// 4. The idx size is read.
+// 5. The bytes for the idx are decompressed using zlib, and the resulting gob is decoded into an in-memory map[string]int64 of the words to offsets.
 //
 // To read a word:
 //
 // 1. The offset is retrieved from the in-memory idx.
 // 2. The file is seeked to the beginning plus the offset.
-// 4. The int64 is read into a variable for the size of the compressed definition.
-// 5. The bytes for the definition are decompressed using zlib, and the resulting gob is decoded into an in-memory *Word.
+// 4. The size of the compressed word is read.
+// 5. The bytes for the word are decompressed using zlib, and the resulting gob is decoded into an in-memory *Word.
 //
 // To create the file:
 // 1. The FileVer is written.
@@ -51,18 +53,37 @@ const FileVer = "DICT3\x00"
 //    b. The Word is encoded as a gob, compressed and written to a temporary buf.
 //    c. The size is written.
 //    d. The buf is written and reset.
-// 4. The current offset is stored and the idx is encoded as a gob, compressed, and written.
-// 5. The file is seeked to the beginning plus the length of FileVer.
-// 6. The idx offset is written.
+// 5. The current offset is stored for the idx offset.
+// 4. A placeholder for the idx size is written.
+// 5. The idx is encoded as a gob, compressed, and written.
+// 6. The idx size is calculated by subtracting the idx offset and the size of the idx size from the current offset.
+// 7. The file is seeked to the idx offset and the idx size is written.
+// 8. The file is seeked to the beginning plus the length of FileVer.
+// 9. The idx offset is written.
 //
 type File struct {
-	mu  sync.Mutex
-	idx map[string]int64
+	idx map[string]size
 	df  interface {
 		io.Reader
-		io.Seeker
 		io.Closer
+		io.ReaderAt
 	}
+}
+
+type size int64
+
+var sizew = int64(binary.Size(size(0)))
+
+// Write writes the size to a writer. It is guaranteed to write sizew
+// bytes on success.
+func (s size) Write(w io.Writer) error {
+	return binary.Write(w, binary.LittleEndian, int64(s))
+}
+
+// Read reads a size from a reader. It is guaranteed to read sizew
+// bytes on success. It should be called like (&s).Read(r).
+func (s *size) Read(r io.Reader) error {
+	return binary.Read(r, binary.LittleEndian, s)
 }
 
 // CreateFile exports a WordMap to a file. The files specified will
@@ -75,18 +96,16 @@ func CreateFile(wm WordMap, dictfile string) error {
 	defer f.Sync()
 	defer f.Close()
 
-	_, err = f.WriteString(FileVer)
-	if err != nil {
+	if _, err := f.WriteString(FileVer); err != nil {
 		return fmt.Errorf("could not write version string: %v", err)
 	}
 
-	err = binary.Write(f, binary.LittleEndian, int64(0))
-	if err != nil {
+	if err := size(0).Write(f); err != nil {
 		return fmt.Errorf("could not write idx offset placeholder: %v", err)
 	}
 
-	idx := map[string]int64{}
-	revidx := map[*Word]int64{}
+	idx := map[string]size{}
+	revidx := map[*Word]size{}
 	buf := new(bytes.Buffer)
 	for k, v := range wm {
 		if cur, ok := revidx[v]; ok {
@@ -98,7 +117,7 @@ func CreateFile(wm WordMap, dictfile string) error {
 
 		cur, err := f.Seek(0, io.SeekCurrent)
 		if err != nil {
-			return fmt.Errorf("could not get current file offset: %v", err)
+			return fmt.Errorf("could not get word offset: %v", err)
 		}
 
 		zw, err := zlib.NewWriterLevel(buf, zlib.BestCompression)
@@ -106,25 +125,22 @@ func CreateFile(wm WordMap, dictfile string) error {
 			return fmt.Errorf("could not compress word: %v", err)
 		}
 
-		err = gob.NewEncoder(zw).Encode(v)
-		if err != nil {
+		if err := gob.NewEncoder(zw).Encode(v); err != nil {
 			return fmt.Errorf("could not encode word: %v", err)
 		}
 
 		zw.Close()
 
-		err = binary.Write(f, binary.LittleEndian, int64(buf.Len()))
-		if err != nil {
+		if err := size(buf.Len()).Write(f); err != nil {
 			return fmt.Errorf("could not write word size: %v", err)
 		}
 
-		_, err = f.Write(buf.Bytes())
-		if err != nil {
+		if _, err := f.Write(buf.Bytes()); err != nil {
 			return fmt.Errorf("could not write word: %v", err)
 		}
 
-		idx[k] = cur
-		revidx[v] = cur
+		idx[k] = size(cur)
+		revidx[v] = size(cur)
 	}
 
 	idxoff, err := f.Seek(0, io.SeekCurrent)
@@ -132,26 +148,40 @@ func CreateFile(wm WordMap, dictfile string) error {
 		return fmt.Errorf("could not get idx offset: %v", err)
 	}
 
+	if err := size(0).Write(f); err != nil {
+		return fmt.Errorf("could not write idx size placeholder: %v", err)
+	}
+
 	zw, err := zlib.NewWriterLevel(f, zlib.BestCompression)
 	if err != nil {
 		return fmt.Errorf("could not compress idx: %v", err)
 	}
 
-	err = gob.NewEncoder(zw).Encode(idx)
-	if err != nil {
+	if err := gob.NewEncoder(zw).Encode(idx); err != nil {
 		return fmt.Errorf("could not encode idx: %v", err)
 	}
 
 	zw.Close() // must be called here before the seek
 
-	_, err = f.Seek(int64(len([]byte(FileVer))), io.SeekStart)
+	idxendoff, err := f.Seek(0, io.SeekCurrent)
 	if err != nil {
+		return fmt.Errorf("could not get current offset: %v", err)
+	}
+
+	if _, err := f.Seek(idxoff, io.SeekStart); err != nil {
+		return fmt.Errorf("could not seek to idx size placeholder: %v", err)
+	}
+
+	if err := size(idxendoff - idxoff).Write(f); err != nil {
+		return fmt.Errorf("could not write idx size: %v", err)
+	}
+
+	if _, err := f.Seek(int64(len([]byte(FileVer))), io.SeekStart); err != nil {
 		return fmt.Errorf("could not seek to idx offset placeholder: %v", err)
 	}
 
-	err = binary.Write(f, binary.LittleEndian, int64(idxoff))
-	if err != nil {
-		return fmt.Errorf("could not write idx size: %v", err)
+	if err := size(idxoff).Write(f); err != nil {
+		return fmt.Errorf("could not write idx offset: %v", err)
 	}
 
 	return nil
@@ -176,18 +206,16 @@ func OpenFile(dictfile string) (*File, error) {
 		return nil, fmt.Errorf("incompatible file versions: expected %#v, got %#v", FileVer, string(buf))
 	}
 
-	var v int64
-	err = binary.Read(d.df, binary.LittleEndian, &v)
-	if err != nil {
+	var idxoff, idxsize size
+	if err := (&idxoff).Read(d.df); err != nil {
 		return nil, fmt.Errorf("could not read idx offset: %v", err)
 	}
 
-	_, err = d.df.Seek(v, io.SeekStart)
-	if err != nil {
-		return nil, fmt.Errorf("could not seek to idx: %v", err)
+	if err := (&idxsize).Read(io.NewSectionReader(d.df, int64(idxoff), sizew)); err != nil {
+		return nil, fmt.Errorf("could not read idx size: %v", err)
 	}
 
-	zr, err := zlib.NewReader(d.df)
+	zr, err := zlib.NewReader(io.NewSectionReader(d.df, int64(idxoff)+sizew, int64(idxsize)))
 	if err != nil {
 		return nil, fmt.Errorf("could not decompress idx: %v", err)
 	}
@@ -207,11 +235,9 @@ func OpenFile(dictfile string) (*File, error) {
 // WARNING: Verify takes a few seconds to run.
 func (d *File) Verify() error {
 	for word, cur := range d.idx {
-		w, err := d.get(cur)
-		if err != nil {
+		if w, err := d.get(cur); err != nil {
 			return fmt.Errorf("failed: %s@%d: %v", word, cur, err)
-		}
-		if w.Word == "" {
+		} else if w.Word == "" {
 			return fmt.Errorf("failed: %s@%d: empty word", word, cur)
 		}
 	}
@@ -223,9 +249,6 @@ func (d *File) Verify() error {
 // clears the in-memory index. Usage of the File afterwards
 // may result in a panic.
 func (d *File) Close() error {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-
 	d.idx = nil
 	return d.df.Close()
 }
@@ -258,30 +281,20 @@ func (d *File) Lookup(word string) (*Word, bool, error) {
 }
 
 // get retrieves the word at the offset in the dict file.
-func (d *File) get(cur int64) (*Word, error) {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-
-	_, err := d.df.Seek(cur, io.SeekStart)
-	if err != nil {
-		return nil, fmt.Errorf("could not seek to %d in db: %v", cur, err)
-	}
-
+func (d *File) get(cur size) (*Word, error) {
 	var n int64
-	err = binary.Read(d.df, binary.LittleEndian, &n)
-	if err != nil {
+	if err := binary.Read(io.NewSectionReader(d.df, int64(cur), int64(binary.Size(n))), binary.LittleEndian, &n); err != nil {
 		return nil, fmt.Errorf("could not get gob length: %v", err)
 	}
 
-	zr, err := zlib.NewReader(io.LimitReader(d.df, n))
+	zr, err := zlib.NewReader(io.NewSectionReader(d.df, int64(cur)+sizew, n))
 	if err != nil {
 		return nil, fmt.Errorf("could not decompress gob: %v", err)
 	}
 	defer zr.Close()
 
 	var w Word
-	err = gob.NewDecoder(zr).Decode(&w)
-	if err != nil {
+	if err := gob.NewDecoder(zr).Decode(&w); err != nil {
 		return nil, fmt.Errorf("could not read gob: %v", err)
 	}
 
